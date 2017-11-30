aa8fee8e9257f745a2ecd72989755fd6b081c374
Kristin Muterspaw <kmmuterspaw@gmail.com>
2017-11-30 10:21:45 -0500

    Update configuration files and scripts for new project architecture. Fix env issue with NODE_ENV=production not being set in production run

--- config/env.js
+++ config/env.js
@@ -3,16 +3,15 @@ const paths = require('./paths')
 
 module.exports = function getEnvVariables(nodeEnv) {
   dotenv.config({ path: paths.appDotEnv })
-  process.env.NODE_ENV = nodeEnv
-
   let env = Object.keys(process.env)
     .filter(key => key.startsWith('APP_'))
     .reduce((e, key) => {
       e[key] = JSON.stringify(process.env[key])
       return e
     }, {})
-  env = { ...env, NODE_ENV: `"${nodeEnv}"` }
-  if (!env.APP_API_URL) { env.APP_API_URL = JSON.stringify('http://localhost:5000/api') }
+
+  env = { ...env, 'process.env.NODE_ENV': JSON.stringify(`${nodeEnv}`) }
+  if (!env.APP_API_URL) { env.APP_API_URL = JSON.stringify('http://0.0.0.0:5000/api') }
 
   return env
 }
