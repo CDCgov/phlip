1a7f7cb33fa61252e367ed6d21c010b0d1269287
Kristin Muterspaw <kmmuterspaw@gmail.com>
2017-12-12 13:28:45 -0500

    Create reducer and action creator tests for data/user

---
+++ src/data/user/__tests__/actions.test.js
@@ -0,0 +1,33 @@
+import * as actions from '../actions'
+import * as types from '../actionTypes'
+
+describe('User action creators', () => {
+  test('should create an action to toggle menu', () => {
+    const expectedAction = {
+      type: types.TOGGLE_MENU,
+      anchor: null
+    }
+    expect(actions.toggleMenu(null)).toEqual(expectedAction)
+  })
+
+  test('should create an action to close menu', () => {
+    const expectedAction = {
+      type: types.CLOSE_MENU
+    }
+    expect(actions.closeMenu()).toEqual(expectedAction)
+  })
+
+  test('should create an action to logout user', () => {
+    const expectedAction = {
+      type: types.LOGOUT_USER
+    }
+    expect(actions.logoutUser()).toEqual(expectedAction)
+  })
+
+  test('should create an action to flush state', () => {
+    const expectedAction = {
+      type: types.FLUSH_STATE
+    }
+    expect(actions.flushState()).toEqual(expectedAction)
+  })
+})
