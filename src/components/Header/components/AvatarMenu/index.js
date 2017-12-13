8e07369d7b94ba80b7ea436215fa4321f0ee3116
Kristin Muterspaw <kmmuterspaw@gmail.com>
2017-12-13 07:44:21 -0500

    Update Logout menu item style to match Admin menu item

--- src/components/Header/components/AvatarMenu/index.js
+++ src/components/Header/components/AvatarMenu/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Grid from 'material-ui/Grid'
-import Avatar from 'components/Avatar'
 import Menu, { MenuItem } from 'material-ui/Menu'
+import Typography from 'material-ui/Typography'
+import Avatar from 'components/Avatar'
 import TextLink from 'components/TextLink'
 
 export const AvatarMenu = ({ role, initials, menuAnchor, open, onCloseMenu, onLogoutUser, onOpenMenu }) => {
@@ -20,7 +21,7 @@ export const AvatarMenu = ({ role, initials, menuAnchor, open, onCloseMenu, onLo
         open={open}
       >
         <MenuItem onClick={onLogoutUser} key="logout-menu">
-          Logout
+          <Typography color="primary" style={{ fontSize: '1rem' }}>Logout</Typography>
         </MenuItem>
         {role === 'Admin' &&
           <MenuItem onClick={onCloseMenu} key="admin-menu">
