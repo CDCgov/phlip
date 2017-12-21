d27a5b2681882ad5b5065473072f752ababc2aa6
Kristin Muterspaw <kmmuterspaw@gmail.com>
2017-12-21 13:11:49 -0500

    Create index for add/edit jurisdiction scene

---
+++ src/scenes/Home/scenes/AddEditJurisdictions/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export class AddEditJurisdictions extends Component {
+  constructor(props, context) {
+    super(props, context)
+  }
+
+  render() {
+    return (
+    )
+  }
+}
