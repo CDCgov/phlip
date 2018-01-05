b4a5c257215cbeb1843c16d14ff922be69490beb
Michael Ta <technical.ta@gmail.com>
2018-01-05 11:26:07 -0500

    Revert "Set environement variables for use in 'Test' box"
    This reverts commit 102a2d3085b400ae829291990fa5804d709e2a25

--- scripts/serve.js
+++ scripts/serve.js
@@ -8,7 +8,7 @@ const APP_PORT = process.env.APP_PORT || 5200
 const APP_API_URL = `${process.env.APP_API_URL}/api` || 'http://backend:80/api'
 
 app.use(express.static('./dist/'))
-app.use('/api', proxy({ target: APP_API_URL, changeOrigin: true }))
+app.use('/api', proxy(APP_API_URL))
 app.use('/', express.static('./dist/index.html'))
 app.use('*', express.static('./dist/index.html'))
 app.listen(APP_PORT, APP_HOST, err => {
