de5a407914f88e166fdd575448bf764a92c22365
Kristin Muterspaw <kmmuterspaw@gmail.com>
2018-01-05 12:23:35 -0500

    Add search bar to app bar header for coding scheme

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,39 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Container, { Column, Row } from 'components/Layout'
 import Icon from 'components/Icon'
 import TextField from 'material-ui/TextField'
 import { InputAdornment } from 'material-ui/Input'
 
-const SearchBar = ({ searchValue, handleSearchValueChange, containerStyles, placeholder, height }) => {
-  const styles = {
-    backgroundColor: '#e5e5e5',
-    padding: '2px 10px'
-  }
-
+const SearchBar = ({ searchValue, handleSearchValueChange, placeholder, ...otherProps }) => {
   return (
-      <TextField
-        value={searchValue}
-        onChange={handleSearchValueChange}
-        placeholder={placeholder}
-        InputProps={{
-          //disableUnderline: true,
-          style: { 'alignItems': 'center'},
-          endAdornment: <InputAdornment style={{ marginTop: 0, height: 24 }} position="end" disableTypography><Icon color="#c6d4da">search</Icon></InputAdornment>
-        }}
-        id="search-bar"
-      />
+    <TextField
+      value={searchValue}
+      onChange={handleSearchValueChange}
+      placeholder={placeholder}
+      InputProps={{
+        style: { 'alignItems': 'center' },
+        endAdornment: <InputAdornment style={{ marginTop: 0, height: 24 }} position="end" disableTypography><Icon color="#c6d4da">search</Icon></InputAdornment>
+      }}
+      id="search-bar"
+      {...otherProps}
+    />
   )
 }
 
 SearchBar.propTypes = {
   searchValue: PropTypes.string,
   handleSearchValueChange: PropTypes.func,
-  height: PropTypes.number
+  placeholder: PropTypes.string
 }
 
-SearchBar.defaultProps = {
-  height: 57
-}
+SearchBar.defaultProps = {}
 
 export default SearchBar
--- src/scenes/CodingScheme/components/Header/index.js
+++ src/scenes/CodingScheme/components/Header/index.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 import Typography from 'material-ui/Typography'
 import Button from 'components/Button'
 import AppBar from 'components/AppBar'
+import SearchBar from 'components/SearchBar'
 
 export const Header = ({ projectName, handleAddQuestion }) => (
   <AppBar>
     <Typography type="title" color="inherit" style={{ flex: 1 }}>
       Coding Scheme | <span style={{ color: '#0faee6' }}>{projectName}</span>
     </Typography>
+    <SearchBar style={{ paddingRight: 10 }} placeholder="Search keywords"/>
     <Button value="+ Add New Question" color="accent" onClick={() => handleAddQuestion() } />
   </AppBar>
 )
