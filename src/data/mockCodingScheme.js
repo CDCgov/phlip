38bcf801ab713dad2f3717d2da6e3b179fdef565
Kristin Muterspaw <kmmuterspaw@gmail.com>
2018-01-10 10:54:23 -0500

    Update mock coding scheme data to use sample questions instead of random text

--- src/data/mockCodingScheme.js
+++ src/data/mockCodingScheme.js
@@ -1,7 +1,3 @@
-import Chance from 'chance'
-
-const chance = new Chance()
-
 export const scheme = [
   {
     type: 'mc',
@@ -12,8 +8,32 @@ export const scheme = [
     children: []
   },
   {
+    type: 'cb',
+    questionBody: 'What animals do the state\'s rabies vaccination laws apply to? (select all that apply)',
+    hint: '',
+    comment: false,
+    answers: [],
+    children: []
+  },
+  {
+    type: 'text',
+    questionBody: 'By what age must initial vaccination occur?',
+    hint: '',
+    comment: false,
+    answers: [],
+    children: []
+  },
+  {
+    type: 'mc',
+    questionBody: 'By what age must subsequent vaccinations occur, or for how long does immunity last?',
+    hint: '',
+    comment: false,
+    answers: [],
+    children: []
+  },
+  {
     type: 'binary',
-    questionBody: chance.paragraph({ sentences: 2 }),
+    questionBody: 'Is a certificate issued after rabies vaccination?',
     hint: '',
     comment: false,
     answers: [],
@@ -21,25 +41,31 @@ export const scheme = [
   },
   {
     type: 'cat',
-    questionBody: chance.paragraph({ sentences: 2 }),
+    questionBody: 'Are tags issued after rabies vaccination?',
     hint: '',
     comment: false,
     answers: [],
     children: []
   },
-  { type: 'cb', questionBody: chance.paragraph({ sentences: 2 }), hint: '', comment: false, answers: [], children: [] },
+  {
+    type: 'cb',
+    questionBody: 'Is rabies vaccination required to obtain a license or registration for the animal?\n',
+    hint: '',
+    comment: false,
+    answers: [],
+    children: []
+  },
   {
     type: 'text',
-    questionBody: chance.paragraph({ sentences: 2 }),
+    questionBody: 'Are there exemptions for vaccination or certificate requirements for animals in the state?',
     hint: '',
     comment: false,
     answers: [],
     children: []
   },
-  { type: 'mc', questionBody: chance.paragraph({ sentences: 2 }), hint: '', comment: false, answers: [], children: [] },
   {
     type: 'binary',
-    questionBody: chance.paragraph({ sentences: 2 }),
+    questionBody: 'Does the state have laws regarding rabies vaccinations for animals entering the state?',
     hint: '',
     comment: false,
     answers: [],
@@ -47,20 +73,18 @@ export const scheme = [
   },
   {
     type: 'cat',
-    questionBody: chance.paragraph({ sentences: 2 }),
+    questionBody: 'Does the state’s law reference any version of the National Association of State Public Health Veterinarians (NASPHV), Compendium of Animal Rabies Prevention and Control (Compendium)?',
     hint: '',
     comment: false,
     answers: [],
     children: []
   },
-  { type: 'cb', questionBody: chance.paragraph({ sentences: 2 }), hint: '', comment: false, answers: [], children: [] },
   {
-    type: 'text',
-    questionBody: chance.paragraph({ sentences: 2 }),
+    type: 'mc',
+    questionBody: 'To what animals do the state\'s rabies vaccination laws for entry apply?',
     hint: '',
     comment: false,
     answers: [],
     children: []
-  },
-  { type: 'mc', questionBody: chance.paragraph({ sentences: 2 }), hint: '', comment: false, answers: [], children: [] }
+  }
 ]
