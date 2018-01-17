d3821e740a4b8c288b0a63ea1289cf4fa019fcd5
Kristin Muterspaw <kmmuterspaw@gmail.com>
2018-01-17 12:47:55 -0500

    Add header and footer to coding scene

---
+++ src/scenes/Coding/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Typography from 'material-ui/Typography'
+import Button from 'components/Button/index'
+import AppBar from 'components/AppBar/index'
+import TextLink from 'components/TextLink/index'
+
+export const Header = ({ projectName, projectId }) => (
+  <AppBar>
+    <Typography type="title" color="inherit" style={{ flex: 1 }}>
+      <span style={{ color: '#0faee6' }}>{projectName}</span> |
+    </Typography>
+    <Button value="View/Edit Protocol" style={{ backgroundColor: 'white', color: 'black', fontWeight: 'bold' }} />
+  </AppBar>
+)
+
+Header.propTypes = {
+  projectName: PropTypes.string,
+  projectId: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+}
+
+export default Header
--- src/scenes/Coding/index.js
+++ src/scenes/Coding/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Container from 'components/Layout'
 
 export class Coding extends Component {
   constructor(props, context) {
@@ -13,11 +16,24 @@ export class Coding extends Component {
   }
 
   render() {
-    return <div></div>
+    return (
+      <Container column flex>
+        <Header projectName={this.props.projectName} projectId={this.props.projectId} />
+        <Container flex style={{ backgroundColor: '#f5f5f5', paddingTop: 25 }}>
+        </Container>
+        <Footer />
+      </Container>
+    )
   }
 }
 
 Coding.propTypes = {
+
 }
 
-export default Coding
+const mapStateToProps = (state, ownProps) => ({
+  projectName: state.scenes.home.main.projects.byId[ownProps.match.params.id].name,
+  projectId: ownProps.match.params.id
+})
+
+export default connect(mapStateToProps)(Coding)
