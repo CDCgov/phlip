7ba5667f5c3e490199f9826569c746838dd0a124
Michael Ta <technicalta@gmail.com>
2018-01-24 11:01:00 -0500

    Update moves 'reorderSchemeRequest' action to 'onMoveNode'

--- src/scenes/CodingScheme/scenes/AddEditQuestion/index.js
+++ src/scenes/CodingScheme/scenes/AddEditQuestion/index.js
@@ -68,10 +68,6 @@ export class AddEditQuestion extends Component {
       })
     }
 
-    // this.questionDefined
-    //   ? this.props.actions.updateQuestionRequest(updatedValues, this.props.projectId, this.questionDefined.id, this.props.location.state.path)
-    //   : this.props.actions.addQuestionRequest(updatedValues, this.props.projectId, this.parentDefined.id || 0)
-
     this.questionDefined ? this.props.actions.updateQuestionRequest(updatedValues, this.props.projectId, this.questionDefined.id, this.props.location.state.path)
       : this.parentDefined ? this.props.actions.addChildQuestionRequest(updatedValues, this.props.projectId, this.parentDefined.id, this.props.location.state.path)
         : this.props.actions.addQuestionRequest(updatedValues, this.props.projectId, 0)
