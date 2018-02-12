6ea7f56afcf211895630c919ae45847e5ba5af5b
Kristin Muterspaw <kmmuterspaw@gmail.com>
2018-02-12 15:30:20 -0500

    Show an error message if the user login fails

--- src/scenes/Login/components/LoginForm/index.js
+++ src/scenes/Login/components/LoginForm/index.js
@@ -10,47 +10,46 @@ import FormTextInput from 'components/FormTextInput'
 import validate from './validate'
 
 let LoginForm = ({ theme, handleSubmit, pristine, reset, error, submitting, children }) => {
-    const bgColor = theme.palette.primary['500']
+  const bgColor = theme.palette.primary['500']
 
-    const headerStyles = {
-        backgroundColor: bgColor,
-        height: 145
-    }
+  const headerStyles = {
+    backgroundColor: bgColor,
+    height: 145
+  }
 
-    const logo = {
-        paddingTop: '35px'
-    }
+  const logo = {
+    paddingTop: '35px'
+  }
 
-    const formStyles = {
-        width: 373,
-        height: 439
-    }
+  const formStyles = {
+    width: 373,
+    height: 439
+  }
 
-    return (
-        <Paper style={formStyles}>
-            <div style={headerStyles}>
-                <Grid container direction="column" alignItems="center" justify="center">
-                    <div style={logo}>
-                        <Logo fontSize="55px" />
-                    </div>
-                </Grid>
-            </div>
-            <form onSubmit={handleSubmit}>
-                {children}
-                <Grid container direction="column" alignItems="center" justify="center">
-                    <Grid item>
-                        <Button type="submit" color="accent" value="Login" disabled={pristine || submitting} />
-                    </Grid>
-                </Grid>
-            </form>
-        </Paper>
-    )
+  return (
+    <Paper style={formStyles}>
+      <div style={headerStyles}>
+        <Grid container direction="column" alignItems="center" justify="center">
+          <div style={logo}>
+            <Logo fontSize="55px" />
+          </div>
+        </Grid>
+      </div>
+      <form onSubmit={handleSubmit}>
+        {children}
+        <Grid container direction="column" alignItems="center" justify="center">
+          <Grid item>
+            <Button type="submit" color="accent" value="Login" disabled={pristine || submitting} />
+          </Grid>
+        </Grid>
+      </form>
+    </Paper>
+  )
 }
 
 LoginForm = reduxForm({
-    form: 'login',
-    validate
+  form: 'login',
+  validate
 })(LoginForm)
-
 
 export default withTheme()(LoginForm)
