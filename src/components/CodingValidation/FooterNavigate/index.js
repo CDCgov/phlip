f92eef60d66c9832a4199741dda7fe7c73c1cc5e
Kristin Muterspaw <kmmuterspaw@gmail.com>
2018-02-16 15:35:30 -0500

    Add 'tab' icon to navigator, and change 'previous' and 'next' question button arrows and color

--- src/components/CodingValidation/FooterNavigate/index.js
+++ src/components/CodingValidation/FooterNavigate/index.js
@@ -6,7 +6,7 @@ import IconButton from 'components/IconButton'
 
 const styles = {
   fontSize: 16,
-  color: '#0faee6'
+  color: '#4eab73'
 }
 
 export const FooterNavigate = ({ currentIndex, getNextQuestion, getPrevQuestion, showNextButton }) => {
@@ -22,14 +22,14 @@ export const FooterNavigate = ({ currentIndex, getNextQuestion, getPrevQuestion,
     <Row displayFlex style={rowStyles}>
       {currentIndex !== 0 &&
       <Row displayFlex style={{ cursor: 'pointer' }} onClick={() => getPrevQuestion(currentIndex - 1)}>
-        <IconButton color="black">arrow_back</IconButton>
+        <IconButton color="#767676">navigate_before</IconButton>
         <Typography type="body2"><span style={{ ...styles, paddingLeft: 5, userSelect: 'none' }}>Previous question</span></Typography>
       </Row>}
       {showNextButton && <Row displayFlex flex={currentIndex === 0} style={{ cursor: 'pointer' }} onClick={() => getNextQuestion(currentIndex + 1)}>
         <Typography type="body2">
           <span style={{ ...styles, paddingRight: 5, userSelect: 'none' }}>Next question</span>
         </Typography>
-        <IconButton color="black">arrow_forward</IconButton>
+        <IconButton color="#767676">navigate_next</IconButton>
       </Row>}
     </Row>
   )
