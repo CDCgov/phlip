c0555d47e3133fc136f8ffa62749f76b5c19f8a5
Kristin Muterspaw <kmmuterspaw@gmail.com>
2018-03-05 10:52:33 -0500

    Initial push for displaying flags in validation screen

--- src/components/CodingValidation/QuestionCard/components/ValidationTable/index.js
+++ src/components/CodingValidation/QuestionCard/components/ValidationTable/index.js
@@ -1,22 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Container, { Column, Row } from 'components/Layout'
-import Card from 'components/Card'
+import { TableBody, TableHead } from 'material-ui/Table'
+import Table from 'components/Table'
+import TableRow from 'components/TableRow'
+import TableCell from 'components/TableCell'
+import Icon from 'components/Icon'
+import Avatar from 'components/Avatar'
+import { getInitials } from 'utils/normalize'
+import Typography from 'material-ui/Typography'
 
+const flagColors = {
+  1: '#2cad73',
+  2: '#fca63a',
+  3: '#d90525'
+}
 
 export const ValidationTable = props => {
   const { mergedUserQuestions } = props
-  console.log(mergedUserQuestions)
 
   return (
-    <Container style={{ backgroundColor: '#f1f7f8', padding: 8 }}>
-      <Row component={<Card />} displayFlex flex>
-
+    <Container column style={{ padding: 12 }}>
+      <Row style={{ paddingBottom: 10 }}><Typography type="title" style={{ color: '#a7bdc6' }}>
+        Flags and Notes
+      </Typography></Row>
+      <Row displayFlex style={{ backgroundColor: '#f1f7f8', padding: 12 }}>
+        <Table>
+          <TableHead>
+            <TableRow style={{ backgroundColor: '#fff' }}>
+              <TableCell>User</TableCell>
+              <TableCell padding="none">Flag</TableCell>
+              <TableCell>Notes</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {mergedUserQuestions.flag.map((flag, i) => (
+              <TableRow key={`flag-${i}`}>
+                <TableCell style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                  <Avatar
+                    cardAvatar
+                    style={{ marginRight: 15 }}
+                    initials={getInitials(flag.raisedBy.firstName, flag.raisedBy.lastName)}
+                    avatarUrl={flag.avatarUrl}
+                  />
+                  <Typography type="caption">{`${flag.raisedBy.firstName} ${flag.raisedBy.lastName}`}</Typography>
+                </TableCell>
+                <TableCell padding="none">
+                  <Icon color={flagColors[flag.type]}>flag</Icon>
+                </TableCell>
+                <TableCell>
+                  <Typography type="caption">{flag.notes}</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Row>
-      <Row></Row>
     </Container>
   )
-
 }
 
 export default ValidationTable
