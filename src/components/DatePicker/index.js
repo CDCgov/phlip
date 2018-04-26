6cf6a9eb010d8543d4b41ab598ba2de6e6091739
Kristin Muterspaw <nlf5@cdc.gov>
2018-04-26 13:49:43 -0400

    Fix issue where jurisdiction dates weren't editable

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FormControl, FormHelperText } from 'material-ui/Form'
 import { DatePicker as MuiDatePicker } from 'material-ui-pickers'
+import moment from 'moment'
 
 export const DatePicker = props => {
   const {
-    label, name, error, dateFormat, disabled, required, value, onChange, ...otherProps
+    label, name, error, dateFormat, disabled, required, value, onChange, onInputChange, ...otherProps
   } = props
+
+  const displayValue = moment(value).format('MM/DD/YYYY') === 'Invalid date' ? value : moment(value).format('MM/DD/YYYY')
 
   return (
     <FormControl error={Boolean(error)} disabled={disabled}>
@@ -16,12 +19,11 @@ export const DatePicker = props => {
         format={dateFormat}
         name={name}
         keyboard
-        invalidDateMessage=""
-        invalidLabel=""
         mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
         onChange={onChange}
         value={value}
         InputLabelProps={{ shrink: true, required, error: Boolean(error) }}
+        InputProps={{ onChange: onInputChange, value: displayValue }}
         {...otherProps}
       />
       <FormHelperText>{error}</FormHelperText>
