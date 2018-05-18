ce012efe65ff74136b2e596f905885ac5abb4c3a
Kristin Muterspaw <nlf5@cdc.gov>
2018-05-18 13:33:02 -0400

    Add id and other required table attributes to the jurisdiction list

--- src/scenes/Home/scenes/AddEditJurisdictions/components/JurisdictionList/index.js
+++ src/scenes/Home/scenes/AddEditJurisdictions/components/JurisdictionList/index.js
@@ -4,15 +4,15 @@ import Table from 'components/Table'
 import { TableBody, TableHead, TableRow, TableCell } from 'material-ui/Table'
 import JurisdictionRow from './components/JurisdictionRow'
 
-export const JurisdictionList = ({ jurisdictions, projectId, onDelete }) => (
-  <Table>
+export const JurisdictionList = ({ jurisdictions, projectId, project, onDelete }) => (
+  <Table summary={`jurisdictions in ${project.name}`}>
     <TableHead>
       <TableRow key="jurisdiction-header">
-        <TableCell key="segment-name">Jurisdiction</TableCell>
-        <TableCell key="segment-start">Segment Start Date</TableCell>
-        <TableCell key="segment-end">Segment End Date</TableCell>
-        <TableCell key="segment-edit">Edit</TableCell>
-        <TableCell key="segment-delete">Delete</TableCell>
+        <TableCell key="segment-name" id="segment-name" scope="col">Jurisdiction</TableCell>
+        <TableCell key="segment-start" id="segment-start-date" scope="col">Segment Start Date</TableCell>
+        <TableCell key="segment-end" id="segment-end-date" scope="col">Segment End Date</TableCell>
+        <TableCell key="segment-edit" id="edit-segment" scope="col">Edit</TableCell>
+        <TableCell key="segment-delete" id="delete-segment" scope="col">Delete</TableCell>
       </TableRow>
     </TableHead>
     <TableBody>
