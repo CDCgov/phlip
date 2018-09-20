643530ba07a5f5f39636e9b8683bba2d8aed8ecf
Kristin Muterspaw <nlf5@cdc.gov>
2018-09-20 15:22:11 -0400

    Add snapshot test for greeting in header

--- src/components/AppHeader/components/UserHeader/components/Greeting/index.js
+++ src/components/AppHeader/components/UserHeader/components/Greeting/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import PropTypes from 'prop-types'
 
-const Greeting = ({ firstName, lastName, role }) => (
+export const Greeting = ({ firstName, lastName, role }) => (
   <Typography style={{ color: 'white' }}>Welcome, {firstName} {lastName}! 
     <span style={{ opacity: 0.6, fontStyle: 'italic' }}>  ({role})</span>
   </Typography>
