2f98bbd97731391b2657f340b776553700b727c1
Kristin Muterspaw <nlf5@cdc.gov>
2018-12-07 14:05:40 -0500

    fix broken popover components

--- src/components/CodingValidation/QuestionCard/components/FlagPopover/components/Popover/index.js
+++ src/components/CodingValidation/QuestionCard/components/FlagPopover/components/Popover/index.js
@@ -1,53 +1,55 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Manager, Popper, Reference } from 'react-popper'
+import FlexGrid from 'components/FlexGrid'
 import Typography from '@material-ui/core/Typography'
 import ClickAwayListener from '@material-ui/core/ClickAwayListener'
-import Grow from '@material-ui/core/Grow'
 import Divider from '@material-ui/core/Divider'
 import IconButton from 'components/IconButton'
-import Card from 'components/Card'
 import { Row } from 'components/Layout'
 
 export const Popover = props => {
   const { target, open, title, onOpen, onClose, children } = props
 
   return (
-    <ClickAwayListener onClickAway={open ? onClose : () => {}}>
-      <Manager>
-        <Reference>
-          {({ ref }) => (
-            <IconButton
-              placement="top"
-              tooltipText={target.tooltip}
-              aria-label={target.tooltip}
-              id={target.id}
-              color={target.color}
-              style={target.style}
-              onClick={onOpen}
-              ref={ref}>
-              {target.icon}
-            </IconButton>
-          )}
-        </Reference>
-        <Popper
-          placement="bottom-end"
-          eventsEnabled={open}
-          style={{ zIndex: open ? 1200 : 0, display: open ? 'flex' : 'none' }}>
-          {({ ref, style, placement, arrowProps }) => (
-            <Grow in={open} ref={ref}>
-              <Card style={{ display: 'flex', flexDirection: 'column', zIndex: open ? 1200 : 0, ...style }}>
+    <Manager>
+      <Reference>
+        {({ ref }) => {
+          return (
+            <div ref={ref}>
+              <IconButton
+                placement="top"
+                tooltipText={target.tooltip}
+                aria-label={target.tooltip}
+                id={target.id}
+                color={target.color}
+                style={target.style}
+                onClick={onOpen}>
+                {target.icon}
+              </IconButton>
+            </div>
+          )
+        }}
+      </Reference>
+      <Popper
+        placement="bottom-end"
+        eventsEnabled={open}
+        style={{ pointerEvents: open ? 'auto' : 'none' }}>
+        {({ ref, style, placement }) => (
+          open && <ClickAwayListener onClickAway={open ? onClose : () => {}}>
+            <div ref={ref} data-placement={placement} style={{ marginTop: 5, ...style, zIndex: 20 }}>
+              <FlexGrid raised container displayFlex>
                 <Row style={{ padding: 16 }}>
                   <Typography variant="body2">{title}</Typography>
                 </Row>
                 <Divider />
                 {children}
-              </Card>
-            </Grow>
-          )}
-        </Popper>
-      </Manager>
-    </ClickAwayListener>
+              </FlexGrid>
+            </div>
+          </ClickAwayListener>
+        )}
+      </Popper>
+    </Manager>
   )
 }
 
