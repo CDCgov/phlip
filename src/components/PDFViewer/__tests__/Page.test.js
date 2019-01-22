acc26fdf9b0470072e5e6ee23fe10a1776dfdb83
Kristin Muterspaw <nlf5@cdc.gov>
2019-01-22 09:12:36 -0500

    Update PDF tests, add Page test for text divs, add test pdf data

--- src/components/PDFViewer/__tests__/Page.test.js
+++ src/components/PDFViewer/__tests__/Page.test.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { Page } from '../Page'
+import { viewport, textContent } from 'utils/testData/pdfTest'
 
 const props = {
   page: {
-    getViewport: () => ({ width: 100, height: 100 })
+    getViewport: () => viewport
   },
   pendingAnnotations: [],
   annotations: [],
-  textContent: {
-    items: []
-  },
+  textContent: { items: [] },
   saveAnnotation: jest.fn(),
   cancelAnnotation: jest.fn(),
   allowSelection: false,
   id: 0,
   viewerDimensions: {
-    height: 0,
-    width: 0
+    height: 760,
+    width: 732
   }
 }
 
@@ -25,5 +24,17 @@ describe('PDFViewer - Page component', () => {
   test('should render correctly', () => {
     const wrapper = mount(<Page {...props} />)
     expect(wrapper).toMatchSnapshot()
+  })
+
+  test('should all text divs', () => {
+    const wrapper = mount(
+      <Page
+        {...props}
+        textContent={textContent}
+      />
+    )
+    const length = textContent.items.length
+    const textLayer = wrapper.find('#text-layer-page-0')
+    expect(textLayer.children()).toHaveLength(length)
   })
 })
