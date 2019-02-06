06d006ffc6f5d5984d97234693d6a595e7c24805
Tim Nguyen <tnmissilen@gmail.com>
2019-02-05 21:26:08 -0500

    switched font-weight for matching text

--- src/components/Autocomplete/index.js
+++ src/components/Autocomplete/index.js
@@ -114,11 +114,11 @@ const renderSuggestion = (suggestion, { query, isHighlighted }) => {
       <div>
         {parts.map((part, index) => {
           return part.highlight ? (
-            <span key={String(index)} style={{ fontWeight: 300 }}>
+            <span key={String(index)} style={{ fontWeight: 500 }}>
               {part.text}
             </span>
           ) : (
-            <strong key={String(index)} style={{ fontWeight: 500 }}>
+            <strong key={String(index)} style={{ fontWeight: 300 }}>
               {part.text}
             </strong>
           )
