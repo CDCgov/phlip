7905710f20cfec29665b0c21c8087ee2cdf9bd60
Tim Nguyen <tnmissilen@gmail.com>
2019-02-11 15:15:05 -0500

    moved coding direction if exist next to the question.  Hover would display tooltip

--- src/scenes/CodingValidation/components/QuestionCard/components/QuestionContent/index.js
+++ src/scenes/CodingValidation/components/QuestionCard/components/QuestionContent/index.js
@@ -11,6 +11,8 @@ import TextFieldQuestions from '../TextFieldQuestions'
 import Button from 'components/Button'
 import ValidationTable from '../ValidationTable'
 import { FlexGrid, Typography } from 'components'
+import Tooltip from 'components/Tooltip'
+import { createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles'
 
 export const QuestionContent = props => {
   const {
@@ -45,12 +47,35 @@ export const QuestionContent = props => {
     areDocsEmpty
   }
 
+  const theme = createMuiTheme({
+    overrides: {
+      MuiTooltip: {
+        tooltip: {
+          fontSize: '2em',
+          color: '#98b3be',
+          backgroundColor: '#f5f5f5',
+          width: 250,
+          maxWidth: 250
+        }
+      }
+    }
+  })
+
   return (
     <Container column flex style={{ flexWrap: 'nowrap', paddingBottom: 15, overflow: 'auto' }}>
       <FlexGrid padding="20px 20px 10px 20px">
         <FlexGrid align="baseline" container type="row">
           <Typography variant="subheading2" style={{ paddingRight: 10 }}>{question.number})</Typography>
           <Typography variant="body2" style={{ letterSpacing: 0 }}>{question.text}</Typography>
+          {question.hint && <MuiThemeProvider theme={theme}>
+            <Tooltip
+              placement='right'
+              title={
+                <Typography variant="body2" style={{ color: '#98b3be' }}><strong>Coding Directions: </strong>{question.hint}</Typography>
+              }>
+              <Icon color="#98b3be" size="14px">lightbulb_outline</Icon>
+            </Tooltip>
+            </MuiThemeProvider>}
         </FlexGrid>
       </FlexGrid>
       <FlexGrid container flex style={{ ...questionAnswerPadding, flexBasis: '60%' }}>
@@ -62,72 +87,72 @@ export const QuestionContent = props => {
         </FlexGrid>
 
         {question.questionType === questionTypes.TEXT_FIELD && mergedUserQuestions === null &&
-        <Column displayFlex style={{ ...answerPadding, paddingRight: 0 }}>
-          <InputBox
-            rows="7"
-            name="text-answer"
-            onChange={onChangeTextAnswer}
-            placeholder="Enter answer"
-            question={question}
-            value={userAnswers.answers[question.possibleAnswers[0].id]}
-            answerId={question.possibleAnswers[0].id}
-            disabled={disableAll}
-            onToggleAnswerForAnno={onToggleAnswerForAnno}
-            enabledAnswerChoice={enabledAnswerChoice}
-            areDocsEmpty={areDocsEmpty}
-          />
-        </Column>}
+          <Column displayFlex style={{ ...answerPadding, paddingRight: 0 }}>
+            <InputBox
+              rows="7"
+              name="text-answer"
+              onChange={onChangeTextAnswer}
+              placeholder="Enter answer"
+              question={question}
+              value={userAnswers.answers[question.possibleAnswers[0].id]}
+              answerId={question.possibleAnswers[0].id}
+              disabled={disableAll}
+              onToggleAnswerForAnno={onToggleAnswerForAnno}
+              enabledAnswerChoice={enabledAnswerChoice}
+              areDocsEmpty={areDocsEmpty}
+            />
+          </Column>}
 
         {question.questionType === questionTypes.TEXT_FIELD && mergedUserQuestions !== null &&
-        <TextFieldQuestions
-          style={{ ...answerPadding, paddingRight: 0 }}
-          mergedUserQuestions={mergedUserQuestions}
-          validatorAnswer={userAnswers.answers[question.possibleAnswers[0].id]}
-          validator={userAnswers.validatedBy}
-          onChange={onChangeTextAnswer}
-          userImages={userImages}
-          question={question}
-          answerId={question.possibleAnswers[0].id}
-          disabled={disableAll}
-          areDocsEmpty={areDocsEmpty}
-        />
+          <TextFieldQuestions
+            style={{ ...answerPadding, paddingRight: 0 }}
+            mergedUserQuestions={mergedUserQuestions}
+            validatorAnswer={userAnswers.answers[question.possibleAnswers[0].id]}
+            validator={userAnswers.validatedBy}
+            onChange={onChangeTextAnswer}
+            userImages={userImages}
+            question={question}
+            answerId={question.possibleAnswers[0].id}
+            disabled={disableAll}
+            areDocsEmpty={areDocsEmpty}
+          />
         }
         <Row style={{ ...answerPadding, paddingRight: 0, paddingTop: 0, paddingBottom: 0 }}>
           {question.includeComment &&
-          <Row>
-            <SimpleInput
-              onChange={onChangeTextAnswer(null, 'comment')}
-              name="comment"
-              shrinkLabel={true}
-              style={{ whiteSpace: 'pre-wrap' }}
-              placeholder="Enter comment"
-              value={comment}
-              rowsMax={3}
-              aria-label="Comment"
-              label="Comment"
-              disabled={disableAll}
-            />
-          </Row>}
+            <Row>
+              <SimpleInput
+                onChange={onChangeTextAnswer(null, 'comment')}
+                name="comment"
+                shrinkLabel={true}
+                style={{ whiteSpace: 'pre-wrap' }}
+                placeholder="Enter comment"
+                value={comment}
+                rowsMax={3}
+                aria-label="Comment"
+                label="Comment"
+                disabled={disableAll}
+              />
+            </Row>}
           {question.isCategoryQuestion &&
-          <Row displayFlex style={{ justifyContent: 'flex-start', paddingTop: 20 }}>
-            <Button
-              onClick={onOpenAlert}
-              style={{ backgroundColor: 'white', color: 'black' }}
-              value="Apply to all tabs"
-            />
-          </Row>}
+            <Row displayFlex style={{ justifyContent: 'flex-start', paddingTop: 20 }}>
+              <Button
+                onClick={onOpenAlert}
+                style={{ backgroundColor: 'white', color: 'black' }}
+                value="Apply to all tabs"
+              />
+            </Row>}
         </Row>
       </FlexGrid>
 
-      {question.hint &&
-      <FlexGrid container type="row" padding="20px 35px 0px 35px" align="center">
-        <Icon color="#98b3be" size="14px">lightbulb_outline</Icon>
-        <Typography variant="body2" style={{ color: '#98b3be' }}>
-          <strong>Coding Directions:</strong>
-          {question.hint}
-        </Typography>
-      </FlexGrid>
-      }
+      {/*{question.hint &&*/}
+      {/*<FlexGrid container type="row" padding="20px 35px 0px 35px" align="center">*/}
+      {/*<Icon color="#98b3be" size="14px">lightbulb_outline</Icon>*/}
+      {/*<Typography variant="body2" style={{ color: '#98b3be' }}>*/}
+      {/*<strong>Coding Directions:</strong>*/}
+      {/*{question.hint}*/}
+      {/*</Typography>*/}
+      {/*</FlexGrid>*/}
+      {/*}*/}
 
       {isValidation && <ValidationTable
         onOpenAlert={onOpenFlagConfirmAlert}
