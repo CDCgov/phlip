a477d95b9530e3ff79751f2ce4461eb8a00e2dfa
Kristin Muterspaw <nlf5@cdc.gov>
2019-02-14 12:20:08 -0500

    Add tests for checking the props are called and icon buttons are displayed

--- src/components/PDFViewer/components/Page/components/Annotation/__tests__/Annotation.test.js
+++ src/components/PDFViewer/components/Page/components/Annotation/__tests__/Annotation.test.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { Annotation } from '../index'
-import { viewport, annotations, totalAnnotationLenth } from 'utils/testData/pdfTest'
+import { viewport, annotations } from 'utils/testData/pdfTest'
 
 const props = {
   annotation: annotations[0],
   index: 0,
-  pageNumber: 1,
+  pageNumber: 0,
   pending: false,
   isClicked: false,
   handleConfirmAnnotation: jest.fn(),
@@ -24,7 +24,55 @@ describe('PDFViewer - Page - Annotation components', () => {
 
   test('should render all annotation rects', () => {
     const wrapper = mount(<Annotation {...props} />)
-    console.log(wrapper.debug())
-    //expect(annotationLayer.children()).toHaveLength(totalAnnotationLenth)
+    expect(wrapper.children()).toHaveLength(3)
+  })
+
+  describe('removing annotations', () => {
+    test('should call handleClickAnnotation if an the first rect of an annotation is clicked', () => {
+      const spy = jest.spyOn(props, 'handleClickAnnotation')
+      const wrapper = mount(<Annotation {...props} isClicked={true} />)
+      wrapper.children().at(0).simulate('click')
+      wrapper.update()
+      expect(spy).toHaveBeenCalled()
+    })
+
+    test('should call handleClickAnnotation if an the last rect of an annotation is clicked', () => {
+      const spy = jest.spyOn(props, 'handleClickAnnotation')
+      const wrapper = mount(<Annotation {...props} isClicked={true} />)
+      wrapper.children().at(2).simulate('click')
+      wrapper.update()
+      expect(spy).toHaveBeenCalled()
+    })
+
+    test('should call handleClickAnnotation if an the any rect of an annotation is clicked', () => {
+      const spy = jest.spyOn(props, 'handleClickAnnotation')
+      const wrapper = mount(<Annotation {...props} isClicked={true} />)
+      wrapper.children().at(1).simulate('click')
+      wrapper.update()
+      expect(spy).toHaveBeenCalled()
+    })
+
+    test('should show x icon button if isClicked is true', () => {
+      const wrapper = mount(<Annotation {...props} isClicked={true} />)
+      expect(wrapper.find('.iconActions')).toHaveLength(1)
+    })
+
+    test('should not show x icon button if isClicked is false', () => {
+      const wrapper = mount(<Annotation {...props} />)
+      expect(wrapper.find('.iconActions')).toHaveLength(0)
+    })
+
+    test('should only show the x button and not the check', () => {
+      const wrapper = mount(<Annotation {...props} isClicked={true} />)
+      expect(wrapper.find('.iconActions').children()).toHaveLength(1)
+    })
+
+    test('should call handleRemoveAnnotation if icon x button is clicked', () => {
+      const spy = jest.spyOn(props, 'handleRemoveAnnotation')
+      const wrapper = mount(<Annotation {...props} isClicked={true} />)
+      wrapper.find('.iconActions').find('button').simulate('click')
+      wrapper.update()
+      expect(spy).toHaveBeenCalled()
+    })
   })
 })
