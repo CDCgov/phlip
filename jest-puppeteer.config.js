21edb2b871f6552df7309a5b947567508dce3bb3
Kristin Muterspaw <nlf5@cdc.gov>
2019-02-15 15:44:52 -0500

    always set headless to false for the purposes of testing to see if this works

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config()
 module.exports = {
   launch: {
     dumpio: true,
-    headless: process.env.HEADLESS === '1' || false,
+    headless: true,
     devtools: false
   }
 }
