e172e1fe914f285e6e7634287fd4bd90c71bc8ef
Kristin Muterspaw <nlf5@cdc.gov>
2019-05-01 14:41:41 -0400

    Add test for checking icon in Coding Scheme action

--- src/scenes/CodingScheme/components/Scheme/components/QuestionNode/__tests__/QuestionNode.test.js
+++ src/scenes/CodingScheme/components/Scheme/components/QuestionNode/__tests__/QuestionNode.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { QuestionNode } from '../index'
+import Icon from 'components/Icon'
 
 const props = {
   listIndex: 0,
@@ -25,7 +26,7 @@ describe('CodingScheme -- Scheme -- QuestionNode', () => {
   test('should render correctly', () => {
     expect(shallow(<QuestionNode {...props} />)).toMatchSnapshot()
   })
-
+  
   describe('Question hovering', () => {
     test('should call setHoveredStatus on mouse enter', () => {
       const wrapper = shallow(<QuestionNode {...props} />)
@@ -35,7 +36,7 @@ describe('CodingScheme -- Scheme -- QuestionNode', () => {
       card.update()
       expect(spy).toHaveBeenCalled()
     })
-
+    
     test('should call turnOffHover on mouse leave', () => {
       const wrapper = shallow(<QuestionNode {...props} />)
       const card = wrapper.find('WithStyles(CardContent)').dive()
@@ -44,24 +45,44 @@ describe('CodingScheme -- Scheme -- QuestionNode', () => {
       card.update()
       expect(spy).toHaveBeenCalled()
     })
-
+    
     test('should display actions if hovering = true and canModify = true', () => {
       const wrapper = shallow(<QuestionNode {...props} node={{ text: 'la la la' }} canModify={true} />)
-
+      
       wrapper.setState({
         hovered: true
       })
       const card = wrapper.find('WithStyles(CardContent)').dive()
       expect(card.find('CardContent').find('WithTheme(Button)')).toHaveLength(3)
     })
-
-    test('should only display edit action if canModify = false and hovering = true', () => {
-      const wrapper = shallow(<QuestionNode {...props} node={{ text: 'la la la' }} canModify={false} />)
-      wrapper.setState({
-        hovered: true
+    
+    describe('hovering without edit mode enabled', () => {
+      test('should only display one action button', () => {
+        const wrapper = shallow(<QuestionNode {...props} node={{ text: 'la la la' }} canModify={false} />)
+        wrapper.setState({
+          hovered: true
+        })
+        const card = wrapper.find('WithStyles(CardContent)').dive()
+        expect(card.find('CardContent').find('WithTheme(Button)')).toHaveLength(1)
       })
-      const card = wrapper.find('WithStyles(CardContent)').dive()
-      expect(card.find('CardContent').find('WithTheme(Button)')).toHaveLength(1)
+  
+      test('should display an eyeball icon in button', () => {
+        const wrapper = shallow(<QuestionNode {...props} node={{ text: 'la la la' }} canModify={false} />)
+        wrapper.setState({
+          hovered: true
+        })
+        const card = wrapper.find('WithStyles(CardContent)').dive()
+        expect(card.find('WithTheme(Button)').prop('value').props.children).toEqual('visibility')
+      })
+      
+      test('should set tooltip text for button icon to be View Question', () => {
+        const wrapper = shallow(<QuestionNode {...props} node={{ text: 'la la la' }} canModify={false} />)
+        wrapper.setState({
+          hovered: true
+        })
+        const card = wrapper.find('WithStyles(CardContent)').dive()
+        expect(card.find('WithStyles(Tooltip)').prop('text')).toEqual('View Question')
+      })
     })
   })
 })
