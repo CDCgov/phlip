d659012425b5ea0e4e6803bed74020fad75221c3
Tim Nguyen <tnmissilen@gmail.com>
2019-05-30 16:25:09 -0400

    added new action and  logic for project search by user id

--- src/data/autocomplete/logic.js
+++ src/data/autocomplete/logic.js
@@ -28,6 +28,7 @@ const getJurisdictionSuggestionsLogic = createLogic({
 const getProjectSuggestionsLogic = createLogic({
   type: `${types.SEARCH_FOR_SUGGESTIONS_REQUEST}_PROJECT`,
   async process({ api, action }, dispatch, done) {
+    console.log('action ',action)
     try {
       // let projects = await api.getProjects({}, {
       let projects = await api.searchProjectList({}, {
@@ -47,7 +48,28 @@ const getProjectSuggestionsLogic = createLogic({
   }
 })
 
+const getProjectsByUserLogic = createLogic({
+  type: types.GET_INITIAL_PROJECT_SUGGESTION_REQUEST,
+  async process({ api, action }, dispatch, done) {
+    try {
+      // let projects = await api.getProjects({}, {
+      let projects = await api.searchProjectListByUser({}, {
+        params: {
+          userId: action.userId,
+          count: action.count
+        }
+      }, {})
+      console.log('by user logic ', projects,' - ', action)
+      dispatch({ type: `${types.SEARCH_FOR_SUGGESTIONS_SUCCESS}_PROJECT`, payload: projects })
+    } catch (err) {
+      dispatch({ type: `${types.SEARCH_FOR_SUGGESTIONS_FAIL}_PROJECT` })
+    }
+    done()
+  }
+})
+
 export default [
   getJurisdictionSuggestionsLogic,
-  getProjectSuggestionsLogic
+  getProjectSuggestionsLogic,
+  getProjectsByUserLogic
 ]
