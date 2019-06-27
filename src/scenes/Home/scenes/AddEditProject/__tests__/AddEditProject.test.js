d50d56b225852f1c1f9869eee732db55b54f413c
Kristin Muterspaw <nlf5@cdc.gov>
2019-06-27 15:08:53 -0400

    Update the reducer tests for Add Edit Project modal

--- src/scenes/Home/scenes/AddEditProject/__tests__/AddEditProject.test.js
+++ src/scenes/Home/scenes/AddEditProject/__tests__/AddEditProject.test.js
@@ -18,31 +18,8 @@ const props = {
   }
 }
 
-const editProps = {
-  ...props,
-  match: {
-    url: '/project/edit/1'
-  },
-  location: {
-    state: {
-      projectDefined: {
-        name: 'Project Name',
-        id: 1,
-        type: 1
-      }
-    }
-  }
-}
-
 describe('Home scene - AddEditProject scene', () => {
   test('should render correctly', () => {
     expect(shallow(<AddEditProject {...props} />)).toMatchSnapshot()
-  })
-
-  test('should only show the last two rows if projectDefined is defined', () => {
-    const details = shallow(<AddEditProject {...editProps} />)
-    const newForm = shallow(<AddEditProject {...props} />)
-    expect(details.find('DetailRow').length).toBe(4)
-    expect(newForm.find('DetailRow').length).toBe(2)
   })
 })
--- src/scenes/Home/scenes/AddEditProject/__tests__/reducer.test.js
+++ src/scenes/Home/scenes/AddEditProject/__tests__/reducer.test.js
@@ -1,47 +1,166 @@
 import { types } from '../../../actions'
-import reducer from '../reducer'
+import reducer, { INITIAL_STATE } from '../reducer'
+
+const getState = (other = {}) => ({
+  ...INITIAL_STATE,
+  ...other
+})
 
 describe('Home scene - AddEditProject reducer', () => {
   test('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual({ goBack: false, formError: null })
+    expect(reducer(undefined, {})).toEqual(INITIAL_STATE)
   })
   
   describe('ADD_PROJECT_REQUEST', () => {
+    const project = { id: 1, name: 'New Project' }
+    const currentState = getState()
+    const action = { type: types.ADD_PROJECT_REQUEST, project }
+    const state = reducer(currentState, action)
+    
     test('should set go back to false', () => {
-      const project = { id: 1, name: 'New Project' }
-      expect(reducer({}, {
-        type: types.ADD_PROJECT_REQUEST,
-        project: project.id
-      }).goBack).toEqual(false)
+      expect(state.goBack).toEqual(false)
+    })
+    
+    test('should set submitting to true', () => {
+      expect(state.submitting).toEqual(true)
     })
   })
   
   describe('UPDATE_PROJECT_REQUEST', () => {
+    const project = { id: 1, name: 'Updated Project' }
+    const currentState = getState()
+    const action = { type: types.UPDATE_PROJECT_REQUEST, project }
+    const state = reducer(currentState, action)
+    
     test('should set go back to false', () => {
-      const project = { id: 1, name: 'New Project' }
-      expect(reducer({}, {
-        type: types.UPDATE_PROJECT_REQUEST,
-        project: project.id
-      }).goBack).toEqual(false)
+      expect(state.goBack).toEqual(false)
+    })
+    
+    test('should set submitting to true', () => {
+      expect(state.submitting).toEqual(true)
     })
   })
   
   describe('DELETE_PROJECT_REQUEST', () => {
+    const project = { id: 1, name: 'Project To Delete' }
+    const currentState = getState()
+    const action = { type: types.DELETE_PROJECT_REQUEST, project }
+    const state = reducer(currentState, action)
+    
     test('should set go back to false', () => {
-      const project = { id: 1, name: 'New Project' }
-      expect(reducer({}, {
-        type: types.DELETE_PROJECT_REQUEST,
-        project: project.id
-      }).goBack).toEqual(false)
+      expect(state.goBack).toEqual(false)
+    })
+    
+    test('should set submitting to true', () => {
+      expect(state.submitting).toEqual(true)
+    })
+  })
+  
+  describe('ADD_PROJECT_SUCCESS', () => {
+    const project = { id: 1, name: 'New Project' }
+    const currentState = getState({ submitting: true })
+    const action = { type: types.ADD_PROJECT_SUCCESS, project }
+    const state = reducer(currentState, action)
+    
+    test('should set go back to true', () => {
+      expect(state.goBack).toEqual(true)
+    })
+    
+    test('should set submitting to false', () => {
+      expect(state.submitting).toEqual(false)
+    })
+  })
+  
+  describe('UPDATE_PROJECT_SUCCESS', () => {
+    const project = { id: 1, name: 'Updated Project' }
+    const currentState = getState({ submitting: true })
+    const action = { type: types.UPDATE_PROJECT_SUCCESS, project }
+    const state = reducer(currentState, action)
+    
+    test('should set go back to true', () => {
+      expect(state.goBack).toEqual(true)
+    })
+    
+    test('should set submitting to false', () => {
+      expect(state.submitting).toEqual(false)
+    })
+  })
+  
+  describe('DELETE_PROJECT_SUCCESS', () => {
+    const project = { id: 1, name: 'Project to delete' }
+    const currentState = getState({ submitting: true })
+    const action = { type: types.DELETE_PROJECT_SUCCESS, project }
+    const state = reducer(currentState, action)
+    
+    test('should set go back to true', () => {
+      expect(state.goBack).toEqual(true)
+    })
+    
+    test('should set submitting to false', () => {
+      expect(state.submitting).toEqual(false)
+    })
+  })
+  
+  describe('ADD_PROJECT_FAIL', () => {
+    const currentState = getState({ submitting: true })
+    const action = { type: types.ADD_PROJECT_FAIL, payload: 'failed to add project' }
+    const state = reducer(currentState, action)
+    
+    test('should set go back to false', () => {
+      expect(state.goBack).toEqual(false)
+    })
+    
+    test('should set submitting to false', () => {
+      expect(state.submitting).toEqual(false)
+    })
+    
+    test('should set form error to the request error', () => {
+      expect(state.formError).toEqual('failed to add project')
     })
   })
   
   describe('UPDATE_PROJECT_FAIL', () => {
-    test('should set the form error to action.payload', () => {
-      expect(reducer({}, {
-        type: types.UPDATE_PROJECT_FAIL,
-        payload: 'Failed to update project'
-      }).formError).toEqual('Failed to update project')
+    const currentState = getState({ submitting: true })
+    const action = { type: types.UPDATE_PROJECT_FAIL, payload: 'failed to update project' }
+    const state = reducer(currentState, action)
+    
+    test('should set go back to false', () => {
+      expect(state.goBack).toEqual(false)
+    })
+    
+    test('should set submitting to false', () => {
+      expect(state.submitting).toEqual(false)
+    })
+    
+    test('should set form error to the request error', () => {
+      expect(state.formError).toEqual('failed to update project')
+    })
+  })
+  
+  describe('DELETE_PROJECT_FAIL', () => {
+    const currentState = getState({ submitting: true })
+    const action = { type: types.DELETE_PROJECT_FAIL, payload: 'failed to delete project' }
+    const state = reducer(currentState, action)
+    
+    test('should set go back to false', () => {
+      expect(state.goBack).toEqual(false)
+    })
+    
+    test('should set submitting to false', () => {
+      expect(state.submitting).toEqual(false)
+    })
+    
+    test('should set form error to the request error', () => {
+      expect(state.formError).toEqual('failed to delete project')
+    })
+  })
+  
+  describe('RESET_FORM_ERROR', () => {
+    test('should reset form error', () => {
+      const currentState = getState({ formError: 'blep' })
+      const action = { type: types.RESET_FORM_ERROR }
+      const state = reducer(currentState, action)
+      expect(state.formError).toEqual(null)
     })
   })
 })
