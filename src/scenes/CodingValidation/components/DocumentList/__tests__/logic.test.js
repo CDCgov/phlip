ebcacf5581bfbaafae7b005f6091e87ebaea275d
Kristin Muterspaw <nlf5@cdc.gov>
2019-07-30 13:36:58 -0400

    Add logic tests for DocumentList redux logic code

--- src/scenes/CodingValidation/components/DocumentList/__tests__/logic.test.js
+++ src/scenes/CodingValidation/components/DocumentList/__tests__/logic.test.js
@@ -6,19 +6,48 @@ import createApiHandler, { docApiInstance } from 'services/api'
 import calls from 'services/api/docManageCalls'
 import { docListPayload } from 'utils/testData/coding'
 import { mockDocuments } from 'utils/testData/documents'
+import { userAnswersCoded, userAnswersValidation, schemeById, mergedUserQuestions } from 'utils/testData/coding'
+
+const userAnswers = {
+  ...userAnswersCoded,
+  1: {
+    ...userAnswersCoded[1],
+    answers: {
+      ...userAnswersCoded[1].answers,
+      123: {
+        ...userAnswersCoded[1][123],
+        annotations: [{ id: 1 }, { id: 2 }, { id: 3 }]
+      }
+    }
+  }
+}
+
+const validationUserAnswers = {
+  ...userAnswersValidation,
+  1: {
+    ...userAnswersValidation[1],
+    answers: {
+      ...userAnswersValidation[1].answers,
+      123: {
+        ...userAnswersValidation[1][123],
+        annotations: [{ id: 1 }, { id: 2 }, { id: 3 }]
+      }
+    }
+  }
+}
 
 describe('CodingValidation - DocumentList logic', () => {
   let mock
-
+  
   const mockReducer = state => state
   const history = {}
   const docApi = createApiHandler({ history }, docApiInstance, calls)
-
+  
   beforeEach(() => {
     mock = new MockAdapter(docApiInstance)
   })
-
-  const setupStore = () => {
+  
+  const setupStore = (otherDoc = {}, otherCoding = {}) => {
     return createMockStore({
       initialState: {
         scenes: {
@@ -26,7 +55,25 @@ describe('CodingValidation - DocumentList logic', () => {
             documentList: {
               documents: {
                 byId: mockDocuments.byId
-              }
+              },
+              ...otherDoc
+            },
+            coding: {
+              question: schemeById[1],
+              page: 'coding',
+              userAnswers,
+              selectedCategoryId: 10,
+              mergedUserQuestions,
+              ...otherCoding
+            }
+          }
+        },
+        data: {
+          user: {
+            currentUser: {
+              id: 1,
+              firstName: 'Test',
+              lastName: 'User'
             }
           }
         }
@@ -38,13 +85,13 @@ describe('CodingValidation - DocumentList logic', () => {
       }
     })
   }
-
+  
   test('should get approved documents and dispatch GET_APPROVED_DOCUMENTS_SUCCESS when successful', done => {
     mock.onGet('/docs/projects/1/jurisdictions/32').reply(200, docListPayload)
-
+    
     const store = setupStore()
     store.dispatch({ type: types.GET_APPROVED_DOCUMENTS_REQUEST, projectId: 1, jurisdictionId: 32, page: 'coding' })
-
+    
     store.whenComplete(() => {
       expect(store.actions).toEqual([
         { type: types.GET_APPROVED_DOCUMENTS_REQUEST, projectId: 1, jurisdictionId: 32, page: 'coding' },
@@ -56,13 +103,13 @@ describe('CodingValidation - DocumentList logic', () => {
       done()
     })
   })
-
+  
   test('should get approved documents and dispatch GET_APPROVED_DOCUMENTS_FAIL on failure', done => {
     mock.onGet('/docs/projects/1/jurisdictions/32').reply(500)
-
+    
     const store = setupStore()
     store.dispatch({ type: types.GET_APPROVED_DOCUMENTS_REQUEST, projectId: 1, jurisdictionId: 32, page: 'coding' })
-
+    
     store.whenComplete(() => {
       expect(store.actions).toEqual([
         { type: types.GET_APPROVED_DOCUMENTS_REQUEST, projectId: 1, jurisdictionId: 32, page: 'coding' },
@@ -86,10 +133,54 @@ describe('CodingValidation - DocumentList logic', () => {
     test('should set the citation as an empty string if it does not exists', done => {
       const store = setupStore()
       store.dispatch({ type: types.ON_SAVE_ANNOTATION, annotation: { docId: 2 } })
-  
+      
       store.whenComplete(() => {
         expect(store.actions[0].citation).toEqual('')
         done()
+      })
+    })
+  })
+  
+  describe('Toggling annotation mode', () => {
+    test('should clear annotations and user\'s if toggling off annotation mode', done => {
+      const store = setupStore()
+      store.dispatch({ type: types.TOGGLE_ANNOTATION_MODE, enabled: false })
+      store.whenComplete(() => {
+        expect(store.actions[0].annotations).toEqual([])
+        expect(store.actions[0].users).toEqual([])
+        done()
+      })
+    })
+    
+    describe('toggling on annotation mode', () => {
+      test('should return the annotations for the current question and selected answer', done => {
+        const store = setupStore()
+        store.dispatch({ type: types.TOGGLE_ANNOTATION_MODE, enabled: true, questionId: 1, answerId: 123 })
+        store.whenComplete(() => {
+          expect(store.actions[0].annotations)
+            .toEqual([
+              { id: 1, fullListIndex: 0, isValidatorAnswer: false, userId: 1 },
+              { id: 2, fullListIndex: 1, isValidatorAnswer: false, userId: 1 },
+              { id: 3, fullListIndex: 2, isValidatorAnswer: false, userId: 1 }
+            ])
+          expect(store.actions[0].users).toEqual([{ userId: 1, isValidator: false }])
+          done()
+        })
+      })
+      
+      test('should return the annotations with the validated by user if the user is on the validation page', done => {
+        const store = setupStore({}, { page: 'validation', userAnswers: validationUserAnswers })
+        store.dispatch({ type: types.TOGGLE_ANNOTATION_MODE, enabled: true, questionId: 1, answerId: 123 })
+        store.whenComplete(() => {
+          expect(store.actions[0].annotations)
+            .toEqual([
+              { id: 1, fullListIndex: 0, isValidatorAnswer: true, userId: 2 },
+              { id: 2, fullListIndex: 1, isValidatorAnswer: true, userId: 2 },
+              { id: 3, fullListIndex: 2, isValidatorAnswer: true, userId: 2 }
+            ])
+          expect(store.actions[0].users).toEqual([{ userId: 2, isValidator: true }])
+          done()
+        })
       })
     })
   })
