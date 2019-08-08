af8a527fbade9adaa5a5eef2262c1cec599ccb98
Tim Nguyen <tnmissilen@gmail.com>
2019-08-08 15:04:16 -0400

    update logout test

--- src/__tests__/logic.test.js
+++ src/__tests__/logic.test.js
@@ -41,7 +41,7 @@ describe('App Root Logic', () => {
       logic
     })
   }
-  
+
   describe('Logging out', () => {
     test('should log the user out', done => {
       const store = setupStore()
@@ -52,7 +52,7 @@ describe('App Root Logic', () => {
         done()
       })
     })
-    
+
     test('should flush redux state', done => {
       const spy = jest.spyOn(persistor, 'purge')
       const store = setupStore()
@@ -62,7 +62,7 @@ describe('App Root Logic', () => {
         done()
       })
     })
-    
+
     describe('if sams authentication is enabled', () => {
       test('should logout the user from the app', done => {
         const store = setupStore()
@@ -70,10 +70,11 @@ describe('App Root Logic', () => {
         store.dispatch({ type: types.LOGOUT_USER })
         store.whenComplete(() => {
           expect(logout.mock.calls.length).toEqual(1)
+          logout.mockClear()
           done()
         })
       })
-      
+
       test('should logout the user in sams', done => {
         const mock = new MockAdapter(axios)
         global.APP_IS_SAML_ENABLED = '1'
