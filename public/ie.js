930306d889c599b9f64a493f8c1763de6f77c76d
Kristin Muterspaw <kmmuterspaw@gmail.com>
2019-09-12 11:11:28 -0400

    Include a separate javascript file to show an alert on internet explorer; separate it from the app.bundle

---
+++ public/ie.js
@@ -0,0 +1,7 @@
+var ua = window.navigator.userAgent;
+var msie = ua.indexOf('MSIE ');
+var trident = ua.indexOf('Trident/');
+var ie = (msie > 0 || trident > 0);
+if (ie) {
+  window.alert('This application will not work in Internet Explorer. Please use Google Chrome.');
+}
